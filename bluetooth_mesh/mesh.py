3597082c4797de4ade363937ded4138fe7e821a7
feversky <fever_sky@qq.com>
2019-03-04 18:48:56 +0800

    unpack of network message and access message

--- bluetooth_mesh/mesh.py
+++ bluetooth_mesh/mesh.py
@@ -25,7 +25,7 @@ import itertools
 import operator
 
 from cryptography.exceptions import InvalidTag
-from bluetooth_mesh.crypto import aes_cmac, aes_ccm, aes_ecb, ApplicationKey
+from bluetooth_mesh.crypto import aes_cmac, aes_ccm, aes_ccm_decrypt, aes_ecb, ApplicationKey
 
 
 class BeaconType(enum.Enum):
@@ -90,7 +90,7 @@ class Nonce:
 
     def network(self, seq, iv_index):
         return bitstring.pack('uint:8, uint:1, uint:7, uintbe:24, uintbe:16, pad:16, uintbe:32',
-                              0x00, self.ctl, self.ttl, seq or self.seq, self.src, iv_index).bytes
+                              0x00, self.ctl, self.ttl, seq, self.src, iv_index).bytes
 
     def application(self, seq, iv_index, szmic=False):
         return bitstring.pack('uint:8, uint:1, pad:7, uintbe:24, uintbe:16, uintbe:16, uintbe:32',
@@ -98,7 +98,7 @@ class Nonce:
 
     def device(self, seq, iv_index, szmic=False):
         return bitstring.pack('uint:8, uint:1, pad:7, uintbe:24, uintbe:16, uintbe:16, uintbe:32',
-                              0x02, szmic, seq or self.seq, self.src, self.dst, iv_index).bytes
+                              0x02, szmic, seq, self.src, self.dst, iv_index).bytes
 
 
 class AccessMessage:
@@ -113,6 +113,9 @@ class AccessMessage:
         self.payload = payload
 
         self.nonce = Nonce(self.src, self.dst, self.ttl, self.ctl)
+
+        self._reassembly = self.__reassembly()
+        next(self._reassembly)
 
     def transport_pdu(self, application_key, seq, iv_index):
         akf = isinstance(application_key, ApplicationKey)
@@ -141,8 +144,50 @@ class AccessMessage:
             yield seq, bitstring.pack('uint:1, uint:1, uint:6, bytes',
                                       seg, akf, aid, upper_transport_pdu).bytes
 
+    def __reassembly(self):
+        buf = []
+        segment = yield
+        while True:
+            try:
+                szmic, seq_zero, seq_o, seq_n, segment = bitstring.BitString(segment).unpack('uint:1, uint:13, uint:5, uint:5, bytes')
+            except ValueError:
+                segment = yield None, None, None
+                continue
+
+            if seq_o == seq_n:
+                buf[seq_o*12:] = segment
+                segment = yield szmic, seq_zero, buf[:seq_n * 12 + len(segment) - 4]
+            elif seq_o > seq_n:
+                segment = yield None, None, None
+            else:
+                buf[seq_o*12:] = segment
+                segment = yield None, None, None
+                
+    def unpack(self, seq, iv_index, payload, app_keys):
+        seg, akf, aid = bitstring.BitString(payload).unpack('uint:1, uint:1, uint:6')
+        if akf:
+            if seg:
+                szmic, seq_zero, packet = self._reassembly.send(payload)
+                if not packet:
+                    return
+            else:
+                szmic = 0
+
+            keys = [key for key in app_keys if key.aid == aid]
+            if keys:
+                app_key = app_keys[0]
+                nonce = self.nonce.application(seq, iv_index, szmic)
+                try:
+                    decrypted = aes_ccm_decrypt(app_key.bytes, nonce, payload[1:], tag_length=8 if szmic else 4)
+                    return decrypted
+                except InvalidTag:
+                    return -1
+
+
 
 class NetworkMessage:
+    PDU_FORMAT = 'pad:6, uint:1, uint:1, bytes:8, uintbe:32'
+
     def __init__(self, message):
         self.message = message
 
@@ -166,3 +211,30 @@ class NetworkMessage:
 
             yield seq, bitstring.pack('uint:1, uint:7, bits, bytes',
                                       iv_index & 1, nid, obfuscated_header, network_pdu).bytes
+
+    @classmethod
+    def unpack(cls, payload, network_keys, local_iv_index):     
+        ivi, nid = bitstring.BitString(payload).unpack('uint:1, uint:7')
+        keys = [key.encryption_keys for key in network_keys if key.encryption_keys[0] == nid]
+        if keys:
+            _, encryption_key, privacy_key = keys[0]
+            obfuscated = payload[1:7]
+            iv_index = local_iv_index if ivi == 0 else local_iv_index - 1
+            privacy_plain = bitstring.pack('pad:40, uintbe:32, bytes:7',
+                                            iv_index, payload[7:14]).bytes
+            pecb = aes_ecb(privacy_key, privacy_plain)[:6]
+            deobfuscated = bytes(map(operator.xor, obfuscated, pecb))
+            ctl, ttl, seq, src = bitstring.BitString(deobfuscated).unpack('uint:1, uint:7, uintbe:24, uintbe:16')
+            net_mic_len = 4 if ctl == 0 else 8
+            net_mic, encrypted = payload[-net_mic_len:], payload[7:-net_mic_len]
+            nonce = Nonce(src, 0, ttl, ctl)
+            try:
+                encrypted = aes_ccm_decrypt(encryption_key,
+                                        nonce.network(seq, iv_index),
+                                        payload[7:],
+                                        tag_length=net_mic_len)
+            except InvalidTag:
+                return
+            dst, transport_pdu = bitstring.BitString(encrypted).unpack('uintbe:16, bytes')
+            return iv_index, ctl, ttl, seq, src, dst, transport_pdu
+        pass
