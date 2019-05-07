c026e726f553aee580aad39ae91b3e9280d869f2
Michał 'Khorne' Lowas-Rzechonek <michal.lowas-rzechonek@silvair.com>
2019-05-07 14:39:13 +0200

    Remove unused utils

--- bluetooth_mesh/messages/util.py
+++ bluetooth_mesh/messages/util.py
@@ -4,8 +4,6 @@ from construct import (
     ExprValidator, ValidationError, obj_
 )
 
-__all__ = ('Reversed', 'RebuildLength', 'SwitchLength', 'MacAddressAdapter', 'BitList')
-
 
 def identity(x):
     return x
@@ -22,55 +20,6 @@ def Reversed(subcon):
         reverse, subcon.sizeof(),
         identity
     )
-
-
-# TODO: is there a better way to calculate size of a nested struct?
-def SwitchLength(type, switch, mapping, path, default=0):
-    def rebuild(type, v):
-        return Rebuild(type, lambda x: len(v.build(path(x))))
-
-    return Switch(
-        switch,
-        {k: rebuild(type, v) for k, v in mapping.items()},
-        default=Rebuild(type, lambda x: default)
-    )
-
-
-def RebuildLength(type, subcons, path):
-    return Rebuild(type, lambda x: len(subcons.build(path(x))))
-
-
-class MacAddressAdapter(Adapter):
-    def _decode(self, obj, context, path):
-        return ':'.join(map('{:02x}'.format, obj))
-
-    def _encode(self, obj, context, path):
-        return bytes(int(i, 16) for i in obj.split(':'))
-
-
-class BitListAdapter(Adapter):
-    def _decode(self, obj, content, path):
-        bits = set()
-
-        for bit, feature in enumerate(obj):
-            if not feature:
-                continue
-
-            bits.add(bit)
-
-        return bits
-
-    def _encode(self, obj, content, path):
-        bits = []
-
-        for bit in range(self.sizeof() * 8):
-            bits.append(bit in obj)
-
-        return bits
-
-
-def BitList(size):
-    return BitListAdapter(Bitwise(Bit[size * 8]))
 
 
 def EnumAdapter(subcon, enum):
