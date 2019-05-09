b93ed1eeea459037be5413adb0c0105b07b42e56
Michał 'Khorne' Lowas-Rzechonek <michal.lowas-rzechonek@silvair.com>
2019-05-09 10:20:37 +0200

    Bump to 0.1.6

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='bluetooth-mesh',
-    version='0.1.5',
+    version='0.1.6',
     author='Michał Lowas-Rzechonek',
     author_email='michal.lowas-rzechonek@silvair.com',
     description=(
