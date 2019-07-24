2a8db445984d71056d26052e0e9b39528016caeb
Rafał Gajda <rafal.gajda@silvair.com>
2019-07-24 16:05:44 +0200

    Added Generic OnOff messages with tests

--- bluetooth_mesh/messages/__init__.py
+++ bluetooth_mesh/messages/__init__.py
@@ -6,10 +6,12 @@ from construct import (
 from .util import Opcode
 from .health import HealthMessage
 from .config import ConfigMessage
+from .generics import GenericOnOff
 
 AccessMessage = Select(
     HealthMessage,
     ConfigMessage,
+    GenericOnOff,
     Struct(
         "opcode" / Opcode,
         "params" / GreedyBytes,
