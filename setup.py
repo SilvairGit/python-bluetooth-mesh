3527ca6ea103bb7f931fc783a0d8abbee36b7558
Rafał Gajda <rafal.gajda@silvair.com>
2019-09-05 09:15:14 +0200

    Initial version of battery messages

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='bluetooth-mesh',
-    version='0.1.20',
+    version='0.1.21',
     author='Michał Lowas-Rzechonek',
     author_email='michal.lowas-rzechonek@silvair.com',
     description=(
