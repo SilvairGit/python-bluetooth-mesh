46227eb552566918ce29ab183d83d19887d08653
Rafał Gajda <rafal.gajda@silvair.com>
2020-03-13 13:01:59 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='bluetooth-mesh',
-    version='0.1.26',
+    version='0.1.27',
     author='Michał Lowas-Rzechonek',
     author_email='michal.lowas-rzechonek@silvair.com',
     description=(
