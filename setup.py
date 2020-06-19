3b75ed361d218b20248afebb2da02d7e905e2b93
Przemysław Fierek <przemyslaw.fierek@silvair.com>
2020-06-19 09:45:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.rst", "r") as f:
 # fmt: off
 setup(
     name='bluetooth-mesh',
-    version='0.2.16rc2',
+    version='0.2.16rc3',
     author_email='michal.lowas-rzechonek@silvair.com',
     description=(
         'Bluetooth mesh for Python'
@@ -45,7 +45,7 @@ setup(
         'construct==2.9.45',
         'cryptography>=2.3.1',
         'crc==0.3.0',
-        'dbus-next@git+https://github.com/SilvairGit/python-dbus-next.git@8737508e495891fd4bd64554b3269ad8d4ef41a2',
+        'dbus-next==0.1.4rc0',
         'ecdsa==0.15',
         'pluggy>=0.13.1',
         'marshmallow>=3.0.1,<4.0',
