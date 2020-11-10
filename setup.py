e1a0e56236bfe7634c0dafdd4e2ebdc283b6b515
Michał Lowas-Rzechonek <michal.lowas-rzechonek@silvair.com>
2020-11-10 11:31:55 +0100

    application: Accept corotuines in Application.connect(addr=...)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.rst", "r") as f:
 # fmt: off
 setup(
     name='bluetooth-mesh',
-    version='0.2.29',
+    version='0.2.30',
     author_email='michal.lowas-rzechonek@silvair.com',
     description=(
         'Bluetooth mesh for Python'
