9ca8e37cd11d7c6d232e66e3eee16df5e9869c9e
Bartosz Flis <bartosz.flis@silvair.com>
2020-12-17 14:06:28 +0100

    v0.4.3
    mergetrain: 8269-add-client-method

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.rst", "r") as f:
 # fmt: off
 setup(
     name='bluetooth-mesh',
-    version='0.4.2',
+    version='0.4.3',
     author_email='michal.lowas-rzechonek@silvair.com',
     description=(
         'Bluetooth mesh for Python'
