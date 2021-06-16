7d23f89c39820dc865ea9bf5dccfca43d51e0eaf
Przemysław Fierek <przemyslaw.fierek@silvair.com>
2021-06-16 13:47:07 +0200

    v0.7.6

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.rst", "r") as f:
 # fmt: off
 setup(
     name='bluetooth-mesh',
-    version='0.7.5',
+    version='0.7.6',
     author_email='michal.lowas-rzechonek@silvair.com',
     description=(
         'Bluetooth mesh for Python'
