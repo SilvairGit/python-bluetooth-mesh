7d359ea40ab5e0da3700ec67ba9a146e1d89cdc6
Wiktor Zbroja <wiktor.zbroja@silvair.com>
2023-06-05 13:26:33 +0200

    Bump cryptography

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     install_requires=[
         'bitstring>=3.1.5,<4.0.0',
         'construct>=2.9.45,<2.9.46',
-        'cryptography>=2.3.1,<3.4',
+        'cryptography==39.0.1',
         'crc==0.3.0',
         'dbus-next>=0.2.1',
         'ecdsa==0.15',
